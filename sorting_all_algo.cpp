5aacdce031c393c56c613e46560e6d1a0d64235b
Suyash Dabral <s.dabral2001@gmail.com>
2023-07-08 22:48:33 +0530

    bubble sort

--- sorting_all_algo.cpp
+++ sorting_all_algo.cpp
@@ -6,6 +6,22 @@ using namespace std;
 
 //Bubble Sort
 
+void bubbleSort(int * arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+
+
 
 
 //Insertion Sort
