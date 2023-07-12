481f627309596cfadef4000603ca194745986503
Suyash Dabral <s.dabral2001@gmail.com>
2023-07-12 23:14:46 +0530

    randomized quick sort

--- sorting_all_algo.cpp
+++ sorting_all_algo.cpp
@@ -98,6 +98,75 @@ class Solution
 //Randomised Quick Sort
 
 
+/* 
+    
+    In randomised quick sort, we just use the rand() function to choose any random element as the pivot. 
+    This method is basically used to improve the O(n^2) time complexity of the worst case of quick sort. 
+
+*/
+
+
+int partition(int arr[], int low, int high)
+{
+    // pivot
+    int pivot = arr[high];
+   
+    // Index of smaller element
+    int i = (low - 1);
+ 
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller
+        // than or equal to pivot
+        if (arr[j] <= pivot) {
+ 
+            // increment index of
+            // smaller element
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+ 
+// Generates Random Pivot, swaps pivot with
+// end element and calls the partition function
+int partition_r(int arr[], int low, int high)
+{
+    // Generate a random number in between
+    // low .. high
+    srand(time(NULL));
+    int random = low + rand() % (high - low);
+ 
+    // Swap A[random] with A[high]
+    swap(arr[random], arr[high]);
+ 
+    return partition(arr, low, high);
+}
+ 
+/* The main function that implements
+QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+ 
+        /* pi is partitioning index,
+        arr[p] is now
+        at right place */
+        int pi = partition_r(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+ 
+
 
 
 
