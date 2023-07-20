110b88a51b33d01a9193a6ec84dd62ac1d2ccd75
Suyash Dabral <s.dabral2001@gmail.com>
2023-07-20 07:06:22 +0530

    building tree from traversals

---
+++ trees_build_tree_from_traversalArray.cpp
@@ -0,0 +1,92 @@
+// Problem Statement: Construct Binary Tree from Preorder and Inorder Traversal
+
+
+//You can practice this in link below
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+node * constructTree(vector < int > & preorder, int preStart, int preEnd, vector 
+ < int > & inorder, int inStart, int inEnd, map < int, int > & mp) {
+  if (preStart > preEnd || inStart > inEnd) return NULL;
+
+  node * root = newNode(preorder[preStart]);
+  int elem = mp[root -> data];
+  int nElem = elem - inStart;
+
+  root -> left = constructTree(preorder, preStart + 1, preStart + nElem, inorder,
+  inStart, elem - 1, mp);
+  root -> right = constructTree(preorder, preStart + nElem + 1, preEnd, inorder, 
+  elem + 1, inEnd, mp);
+
+  return root;
+}
+
+node * buildTree(vector < int > & preorder, vector < int > & inorder) {
+  int preStart = 0, preEnd = preorder.size() - 1;
+  int inStart = 0, inEnd = inorder.size() - 1;
+
+  map < int, int > mp;
+  for (int i = inStart; i <= inEnd; i++) {
+    mp[inorder[i]] = i;
+  }
+
+  return constructTree(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+}
+
+int main() {
+
+  vector<int> preorder{10,20,40,50,30,60};
+  vector<int> inorder{40,20,50,10,60,30};
+  node * root = buildTree(preorder, inorder);
+  return 0;
+}
+
+
+
+//Construct Binary Tree from Inorder and Postorder Traversal
+
+
+
+/*
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+         if (inorder.size() != postorder.size())
+            return NULL;
+        map<int,int> hm;
+        for (int i=0;i<inorder.size();++i)
+            hm[inorder[i]] = i;
+        return buildTreePostIn(inorder, 0, inorder.size()-1, postorder, 0, 
+                              postorder.size()-1,hm);
+    }
+    TreeNode* buildTreePostIn(vector<int> &inorder, int is, int ie, vector<int> &postorder, int ps, int pe, 
+                                     map<int,int> &hm){
+        if (ps>pe || is>ie) return NULL;
+        TreeNode* root = new TreeNode(postorder[pe]);
+        int ri = hm[postorder[pe]];
+        TreeNode* leftchild = buildTreePostIn(inorder, is, ri-1, postorder, ps, ps+ri-is-1, hm);
+        TreeNode* rightchild = buildTreePostIn(inorder,ri+1, ie, postorder, ps+ri-is, pe-1, hm);
+        root->left = leftchild;
+        root->right = rightchild;
+        return root;
+    }
+};
+
+*/
