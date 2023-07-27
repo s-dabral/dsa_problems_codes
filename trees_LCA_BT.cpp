b1e94f7d095460b8c9d6d2bf3fdb5aae90b5c8c5
Suyash Dabral <s.dabral2001@gmail.com>
2023-07-27 07:36:12 +0530

    LCA_BT

---
+++ trees_LCA_BT.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Function to find the Lowest Common Ancestor in a Binary Tree
+
+
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL || root==p || root==q)
+        {
+            return root;
+        }
+
+        TreeNode *left=lowestCommonAncestor(root->left,p,q);
+        TreeNode * right=lowestCommonAncestor(root->right,p,q);
+
+        if(left==NULL){
+            return right;
+        }
+
+        else if(right==NULL){
+            return left;
+        }
+
+       else {
+           return root;
+       }
+    }
+};
