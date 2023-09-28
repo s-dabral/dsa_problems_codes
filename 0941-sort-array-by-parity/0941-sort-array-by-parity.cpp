464e0b6c6080858ed65bd5090e1611727e9dddb9
Suyash Dabral <101198978+s-dabral@users.noreply.github.com>
2023-09-28 18:27:38 +0530

    Time: 3 ms (96.18%), Space: 17.4 MB (6.46%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
       
-        unordered_map<int, vector<int>> map1; // Create an unordered_map to store the numbers based on their parity
+        unordered_map<int, vector<int>> map1; 
 
-        // Iterate through the input vector nums
         for(auto x : nums){
-            map1[x % 2].push_back(x); // Group the numbers based on their parity using modulo operator and store them in the map
+            map1[x % 2].push_back(x); 
         }
-
-        // Check if there are no odd numbers in the input vector
         if(map1.find(1) == map1.end())
-            return map1[0]; // Return the vector of even numbers
+            return map1[0]; 
 
-        // Check if there are no even numbers in the input vector
         if(map1.find(0) == map1.end())
-            return map1[1]; // Return the vector of odd numbers
-
-        // Concatenate the vectors of even and odd numbers and store the result in the vector ret
+            return map1[1]; 
         map1[0].insert(map1[0].end(), map1[1].begin(), map1[1].end());
 
-        return map1[0]; // Return the sorted vector
+        return map1[0];
     }
 };
